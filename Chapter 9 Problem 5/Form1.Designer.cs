7c1019b2c0b222883e0f9a24f5163f58a67c3b2f
Connor <Blackhat.Gaming@icloud.com>
2017-03-13 15:29:50 -0400

    Chapter 9 Problem 5 Completed

---
+++ Chapter 9 Problem 5/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Chapter_9_Problem_5
+{
+    partial class TripCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Accept = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Destination = new System.Windows.Forms.TextBox();
+            this.Gallons = new System.Windows.Forms.TextBox();
+            this.Miles = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Accept
+            // 
+            this.Accept.Location = new System.Drawing.Point(129, 158);
+            this.Accept.Name = "Accept";
+            this.Accept.Size = new System.Drawing.Size(75, 23);
+            this.Accept.TabIndex = 0;
+            this.Accept.Text = "Accept";
+            this.Accept.UseVisualStyleBackColor = true;
+            this.Accept.Click += new System.EventHandler(this.Accept_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(129, 187);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 1;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Destination
+            // 
+            this.Destination.Location = new System.Drawing.Point(175, 30);
+            this.Destination.Name = "Destination";
+            this.Destination.Size = new System.Drawing.Size(100, 20);
+            this.Destination.TabIndex = 2;
+            // 
+            // Gallons
+            // 
+            this.Gallons.Location = new System.Drawing.Point(175, 66);
+            this.Gallons.Name = "Gallons";
+            this.Gallons.Size = new System.Drawing.Size(100, 20);
+            this.Gallons.TabIndex = 3;
+            // 
+            // Miles
+            // 
+            this.Miles.Location = new System.Drawing.Point(175, 102);
+            this.Miles.Name = "Miles";
+            this.Miles.Size = new System.Drawing.Size(100, 20);
+            this.Miles.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Destination:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Gallons used:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(64, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Miles Traveled:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(97, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 27);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Trip Calculator";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(67, 236);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(0, 13);
+            this.Result.TabIndex = 9;
+            // 
+            // TripCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(339, 280);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Miles);
+            this.Controls.Add(this.Gallons);
+            this.Controls.Add(this.Destination);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Accept);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Location = new System.Drawing.Point(500, 250);
+            this.Name = "TripCalculator";
+            this.Text = "Trip Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Accept;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox Destination;
+        private System.Windows.Forms.TextBox Gallons;
+        private System.Windows.Forms.TextBox Miles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Result;
+    }
+}
+
---
+++ Chapter 9 Problem 5/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_9_Problem_5
+{
+    public partial class TripCalculator : Form
+    {
+        public TripCalculator()
+        {
+            InitializeComponent();
+        }
+        private void Accept_Click(object sender, EventArgs e)
+        {
+            string city;
+            double miles = 0;
+            double gallons = 0;
+            city = Destination.Text;
+            string enteredMiles = Miles.Text;
+            string enteredGallons = Gallons.Text;
+            //Parsing
+            bool valid = double.TryParse(enteredMiles, out miles);
+            bool check = double.TryParse(enteredGallons, out gallons);
+            //check
+            if(valid && check && miles >= 0 && gallons >= 0)
+            {
+                Result.Left = 25;
+                Result.Text = city + " gave you an average miles/gallon of " + 
+                    Convert.ToString(miles / gallons);
+            }
+        }
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Destination.Text = "";
+            Miles.Text = "";
+            Gallons.Text = "";
+            Result.Text = "";
+        }
+    }
+}
